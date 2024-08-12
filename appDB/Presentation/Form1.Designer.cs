dbbae9fc75f24076fb5b543862244f061a9af653
Evan Duarte <e.efrenduarte@gmail.com>
2024-08-11 19:03:35 -0600

    Main Screen

---
+++ appDB/Presentation/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Presentation
+{
+    partial class Reportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnMainContainer = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnTopNav = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnleftSide = new System.Windows.Forms.Panel();
+            this.panelEmptyTopMenu = new System.Windows.Forms.Panel();
+            this.pnReportsCTA = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnMain.SuspendLayout();
+            this.pnMainContainer.SuspendLayout();
+            this.pnleftSide.SuspendLayout();
+            this.pnReportsCTA.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnMain.Controls.Add(this.pnMainContainer);
+            this.pnMain.Controls.Add(this.pnTopNav);
+            this.pnMain.Controls.Add(this.pnleftSide);
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(1306, 630);
+            this.pnMain.TabIndex = 0;
+            // 
+            // pnMainContainer
+            // 
+            this.pnMainContainer.Controls.Add(this.label1);
+            this.pnMainContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMainContainer.Location = new System.Drawing.Point(175, 49);
+            this.pnMainContainer.Name = "pnMainContainer";
+            this.pnMainContainer.Size = new System.Drawing.Size(1131, 581);
+            this.pnMainContainer.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(483, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Proyecto Base de datos";
+            // 
+            // pnTopNav
+            // 
+            this.pnTopNav.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pnTopNav.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTopNav.Location = new System.Drawing.Point(175, 0);
+            this.pnTopNav.Name = "pnTopNav";
+            this.pnTopNav.Size = new System.Drawing.Size(1131, 49);
+            this.pnTopNav.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sans Serif Collection", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(51, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Reportes";
+            // 
+            // pnleftSide
+            // 
+            this.pnleftSide.BackColor = System.Drawing.Color.Teal;
+            this.pnleftSide.Controls.Add(this.pnReportsCTA);
+            this.pnleftSide.Controls.Add(this.panelEmptyTopMenu);
+            this.pnleftSide.Controls.Add(this.panel1);
+            this.pnleftSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnleftSide.Location = new System.Drawing.Point(0, 0);
+            this.pnleftSide.Name = "pnleftSide";
+            this.pnleftSide.Size = new System.Drawing.Size(175, 630);
+            this.pnleftSide.TabIndex = 0;
+            // 
+            // panelEmptyTopMenu
+            // 
+            this.panelEmptyTopMenu.BackColor = System.Drawing.Color.Teal;
+            this.panelEmptyTopMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelEmptyTopMenu.Location = new System.Drawing.Point(0, 49);
+            this.panelEmptyTopMenu.Name = "panelEmptyTopMenu";
+            this.panelEmptyTopMenu.Size = new System.Drawing.Size(175, 32);
+            this.panelEmptyTopMenu.TabIndex = 1;
+            // 
+            // pnReportsCTA
+            // 
+            this.pnReportsCTA.BackColor = System.Drawing.Color.Teal;
+            this.pnReportsCTA.Controls.Add(this.btnReports);
+            this.pnReportsCTA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnReportsCTA.Location = new System.Drawing.Point(0, 81);
+            this.pnReportsCTA.Name = "pnReportsCTA";
+            this.pnReportsCTA.Size = new System.Drawing.Size(175, 48);
+            this.pnReportsCTA.TabIndex = 2;
+            this.pnReportsCTA.Click += new System.EventHandler(this.pnReportsCTA_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 49);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.Teal;
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Roboto Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.White;
+            this.btnReports.Image = global::Presentation.Properties.Resources.Reports;
+            this.btnReports.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReports.Location = new System.Drawing.Point(3, 3);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(169, 42);
+            this.btnReports.TabIndex = 0;
+            this.btnReports.Text = "        Dashboard";
+            this.btnReports.UseVisualStyleBackColor = false;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Presentation.Properties.Resources.dahboard32pxBlack;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Reportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 630);
+            this.Controls.Add(this.pnMain);
+            this.Name = "Reportes";
+            this.Text = "Form1";
+            this.pnMain.ResumeLayout(false);
+            this.pnMainContainer.ResumeLayout(false);
+            this.pnMainContainer.PerformLayout();
+            this.pnleftSide.ResumeLayout(false);
+            this.pnReportsCTA.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnTopNav;
+        private System.Windows.Forms.Panel pnleftSide;
+        private System.Windows.Forms.Panel pnMainContainer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelEmptyTopMenu;
+        private System.Windows.Forms.Panel pnReportsCTA;
+        private System.Windows.Forms.Button btnReports;
+    }
+}
+
---
+++ appDB/Presentation/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class Reportes : Form
+    {
+        public Reportes()
+        {
+            InitializeComponent();
+        }
+
+        private void pnReportsCTA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mostrando reportes", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        // Supongamos que pnReportsCTA es el nombre de un formulario, no un panel
+
+        private Form frmMetric = new frmMetrics(); 
+
+        private void btnReports_Click(object sender, EventArgs e)
+        {
+            openReportsChild(this.frmMetric);
+        }
+
+        private void openReportsChild(object formChild)
+        {
+            Form pnlSale = formChild as Form;
+            if (pnlSale != null) // Asegúrate de que pnlSale no sea null
+            {
+                if (this.pnMainContainer.Controls.Count > 0)
+                    this.pnMainContainer.Controls.RemoveAt(0);
+
+                pnlSale.TopLevel = false;
+                pnlSale.Dock = DockStyle.Fill;
+                this.pnMainContainer.Controls.Add(pnlSale);
+                this.pnMainContainer.Tag = pnlSale;
+                pnlSale.Show();
+            }
+            else
+            {
+                MessageBox.Show("El formulario no es válido.");
+            }
+        }
+
+
+    }
+}
